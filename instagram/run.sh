739f658b76ce081c82e4bb0173989eed814eb1c3
dockerised <43545848+dockerised@users.noreply.github.com>
2022-07-12 14:41:09 +0100

    Update run.sh

--- instagram/run.sh
+++ instagram/run.sh
@@ -1,3 +1,4 @@
+username=$1
 sudo -i
 pip3 install instaloader==4.7.1
-/home/george/.local/bin/instaloader --login=miller11_2019 --dirname-pattern={profile} --filename-pattern={date_utc:%Y}/{shortcode} ":saved"
+~/.local/bin/instaloader --login=$username --dirname-pattern={profile} --filename-pattern={date_utc:%Y}/{shortcode} ":saved"
